e23ff67f0116c0420b0c07e269d2118ed6850afc
srikant <srikantvs26@gmail.com>
2021-10-03 02:50:32 +0530

    Indentation Changes

--- java-8/practice/com/srikantvs/java8/streams/Streams02FlatMapDemo.java
+++ java-8/practice/com/srikantvs/java8/streams/Streams02FlatMapDemo.java
@@ -19,33 +19,34 @@ public class Streams02FlatMapDemo {
 
         //		use case : I want to get all the activities of students.
         List<String> studentActivities = StudentDataBase.getAllStudents()
-                                                        .stream()
-                                                        .map(eachStudent -> eachStudent.getActivities())//Stream<List<String>>
-                                                        .flatMap(takeThatStudentList -> takeThatStudentList.stream())// here. it will become <Stream<Stream<String>> but with that flatmap one stream will be gone so we will be end up with just Stream<String>
-                                                        .collect(Collectors.toList());
+                                                    .stream()
+                                                    .map(eachStudent -> eachStudent.getActivities())//Stream<List<String>>
+                                                    .flatMap(takeThatStudentList -> takeThatStudentList.stream())// here. it will become <Stream<Stream<String>> but with that flatmap one stream will be gone so we will be end up with just Stream<String>
+                                                    .collect(Collectors.toList());
 
         System.out.println(studentActivities);
+
 
 
         //		usecase : I want student activities which are unique.
 
         Set<String> uniqueStudentActivities = StudentDataBase.getAllStudents()
-                                                                .stream()
-                                                                .map(eachStudent -> eachStudent.getActivities())//Stream<List<String>>
-                                                                .flatMap(takeThatStudentList -> takeThatStudentList.stream())// here. it will become <Stream<Stream<String>> but with that flatmap one stream will be gone so we will be end up with just Stream<String>
-                                                                .collect(Collectors.toSet());
+                                                    .stream()
+                                                    .map(eachStudent -> eachStudent.getActivities())//Stream<List<String>>
+                                                    .flatMap(takeThatStudentList -> takeThatStudentList.stream())// here. it will become <Stream<Stream<String>> but with that flatmap one stream will be gone so we will be end up with just Stream<String>
+                                                    .collect(Collectors.toSet());
 
         System.out.println(uniqueStudentActivities);
 
         List<String> uniqueStudentActivities2 = StudentDataBase.getAllStudents()
-                                                                .stream()
-                                                                .map(eachStudent -> eachStudent.getActivities())//Stream<List<String>>
-                                                                .flatMap(takeThatStudentList -> takeThatStudentList.stream())// here. it will become <Stream<Stream<String>> but with that flatmap one stream will be gone so we will be end up with just Stream<String>
-                                                                .distinct()
-                                                                .sorted()
-                                                                .collect(Collectors.toList());
+                                                        .stream()
+                                                        .map(eachStudent -> eachStudent.getActivities())//Stream<List<String>>
+                                                        .flatMap(takeThatStudentList -> takeThatStudentList.stream())// here. it will become <Stream<Stream<String>> but with that flatmap one stream will be gone so we will be end up with just Stream<String>
+                                                        .distinct()
+                                                        .sorted()
+                                                        .collect(Collectors.toList());
 
         System.out.println(uniqueStudentActivities2);
-        
+
     }
 }
--- java-8/practice/com/srikantvs/java8/streams/Streams11FindFirstAnyDemo.java
+++ java-8/practice/com/srikantvs/java8/streams/Streams11FindFirstAnyDemo.java
@@ -7,34 +7,27 @@ import com.learnJava.data.StudentDataBase;
 
 public class Streams11FindFirstAnyDemo {
 
-	public static void main(String[] args) {
-		
-		
-		/*
-		 * findFirst and findAny method is used to find an element from stream to work with.
-		 * both return optional object.
-		 * 
-		 * 
-		 * findFirst : find the first element for us.
-		 * findAny : find any element for us.
-		 */
-		
-		
-		Optional<Student> student1 = StudentDataBase.getAllStudents()
-											.stream()
-											.findAny();
-		
-		student1.ifPresent(student->System.out.println(student));
-		
-		Optional<Student> student2 = StudentDataBase.getAllStudents()
-											.stream()
-											.findFirst();
-		
-		student2.ifPresent(student->System.out.println(student));
-		
-		
-		
-		
-	}
-	
+    public static void main(String[] args) {
+
+        /*
+         * findFirst and findAny method is used to find an element from stream
+         * to work with. both return optional object.
+         * 
+         * 
+         * findFirst : find the first element for us. findAny : find any element
+         * for us.
+         */
+
+        Optional<Student> student1 = StudentDataBase.getAllStudents().stream()
+                .findAny();
+
+        student1.ifPresent(student -> System.out.println(student));
+
+        Optional<Student> student2 = StudentDataBase.getAllStudents().stream()
+                .findFirst();
+
+        student2.ifPresent(student -> System.out.println(student));
+
+    }
+
 }
