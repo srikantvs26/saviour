bff0f290d82fa45b14cffcea88f872dd580726c3
srikant <srikantvs26@gmail.com>
2021-10-16 20:20:46 +0530

    method references examples added.

---
+++ java-8/practice/com/srikantvs/java8/methodreferences/MethodReference002Demo.java
@@ -0,0 +1,36 @@
+package com.srikantvs.java8.methodreferences;
+
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import com.learnJava.data.Student;
+
+public class MethodReference002Demo {
+
+    public static void main(String[] args) {
+        
+        // Type: a->a.method()     ==>        Class::method
+        // Type: (a,b)->a.method(b) ==>       Class::method
+        
+        
+        
+        Function<String, String> upperCaseLambda = s->s.toUpperCase();
+        Function<String, String> upperCaseMR = String::toUpperCase;
+        
+        System.out.println(upperCaseLambda.apply("elias samson"));
+        System.out.println(upperCaseMR.apply("elias samson"));
+        
+        //-------------------------------------------------------------------
+        
+        Comparator<String> stringComparatorLE = (s1,s2)->s1.compareTo(s2);
+        Comparator<String> stringComparatorMR =String::compareTo;
+        
+        System.out.println(stringComparatorLE.compare("Rabbit", "Rabbit"));
+        System.out.println(stringComparatorMR.compare("Lion", "Rabbit"));
+        
+        
+        
+    }
+
+}
