eca75a54b04143b4b8e23d00aac5c4b815f57b31
srikant <srikantvs26@gmail.com>
2021-10-22 16:15:53 +0530

    Date time formatting.

---
+++ java-8/practice/com/srikantvs/java8/itsssstime/DateTime037DateTimeFormatterLocalTimeDemo.java
@@ -0,0 +1,41 @@
+package com.srikantvs.java8.itsssstime;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTime037DateTimeFormatterLocalTimeDemo {
+
+    public static void main(String[] args) {
+        
+        //formatting : 
+        
+        LocalTime localTime = LocalTime.NOON;
+        System.out.println("localTime : "+localTime);
+        
+        String timeFormat1 = localTime.format(DateTimeFormatter.ISO_TIME);
+        System.out.println("timeFormat1 : "+timeFormat1);
+        
+        
+        // parsing v1
+        
+        String time = "16:55";
+        LocalTime localTime2 = LocalTime.parse(time);
+        System.out.println("time : "+time);
+        System.out.println("localTime2 : "+localTime2);
+        
+        
+        // parsing v2
+        String time2 = "04:14 PM";//hh:mm a
+        LocalTime localTime3 = LocalTime.parse(time2, DateTimeFormatter.ofPattern("hh:mm a"));
+        System.out.println("time2 : "+time2);
+        System.out.println("localTime3 : "+localTime3);
+        
+        
+    }
+
+
+
+
+
+}
