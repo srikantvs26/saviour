bf313ed3ecffc39378b9bfa0c33b03b66218c3af
srikant <srikantvs26@gmail.com>
2021-10-23 12:34:11 +0530

    duplicate code and practice folder removed.

---
+++ java-8/src/com/srikantvs/java8/optional/Optional005OrElseGetDemo.java
@@ -0,0 +1,36 @@
+package com.srikantvs.java8.optional;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import com.srikantvs.java8.data.Student;
+
+public class Optional005OrElseGetDemo {
+    
+    private static String getStudentName(Student student1) {
+        String studentName = Optional.of(student1)
+                                     .map(singleStudent->singleStudent.getName())
+                                     .orElseGet(()->"John Doe");
+        return studentName;
+    }
+    
+    public static void main(String[] args) {
+        
+        //orElseGet example.
+        // orElseGet takes a supplier.
+        
+        Student student1 = new Student("Adam",2,3.6, "male",10,Arrays.asList("swimming", "basketball","volleyball"));
+
+        // Now I want this student Name if name is not there I want name as John Doe.
+        
+        String studentName = getStudentName(student1);
+        System.out.println(studentName);
+        
+        student1.setName(null);// Now we are passing name as null
+        String studentName2 = getStudentName(student1);
+        System.out.println(studentName2);
+        
+    }
+
+   
+}
