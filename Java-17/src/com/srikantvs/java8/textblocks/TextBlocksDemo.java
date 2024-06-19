147db3686f57ae4a545f27adedce2d815c05b6d4
Srikant V S <38664247+srikantvs26@users.noreply.github.com>
2024-06-19 05:40:50 +0000

    dslkj

---
+++ Java-17/src/com/srikantvs/java8/textblocks/TextBlocksDemo.java
@@ -0,0 +1,31 @@
+package com.srikantvs.java8.textblocks;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TextBlocksDemo {
+    // find /home/codespace/.m2/repository/com/fasterxml/ -name "*.jar" -exec cp {} ./lib/ \;
+    public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String data = "{\r\n"
+                + "\"Name\" : \"Java\","
+                + "\n\"RollNO\" : \"20\"\r\n"
+                + "}";
+
+        String jsonDataWithTextBlocks = """
+                {
+                    "companyName": "companyName",
+                    "description": "description"
+                }
+                """;
+
+        JsonNode result1 = objectMapper.readTree(jsonDataWithTextBlocks);
+        System.out.println(result1.toPrettyString());
+
+        JsonNode result2 = objectMapper.readTree(data);
+        System.out.println(result2.toPrettyString());
+    }
+
+}
