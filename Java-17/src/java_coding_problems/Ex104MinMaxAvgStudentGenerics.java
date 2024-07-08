8f8d244cf8911d206624a5593c235eca90d31477
Srikant V S <38664247+srikantvs26@users.noreply.github.com>
2024-07-08 16:00:04 +0000

    utg

--- Java-17/src/java_coding_problems/Ex104MinMaxAvgStudentGenerics.java
+++ Java-17/src/java_coding_problems/Ex104MinMaxAvgStudentGenerics.java
@@ -21,17 +21,18 @@ public class Ex104MinMaxAvgStudentGenerics {
         Student[] students = { s1, s2, s3 };
 
         Comparator<Student> byWeight = Comparator.comparingInt(s -> s.getWeight());
-        
+
         Student heaviestStudent = getHeaviestStudent(students, byWeight);
-        System.out.println("heaviest student :: "+heaviestStudent);
+        System.out.println("heaviest student :: " + heaviestStudent);
         Student heaviestStudentV1 = getHeaviestStudentV1(students);
-        System.out.println("heaviest student :: "+heaviestStudentV1);
+        System.out.println("heaviest student :: " + heaviestStudentV1);
 
     }
 
     /**
-     * T is Student 
+     * T is Student
      * getHeaviestStudentV2 method we can pass Array of Students, A comparator of Student or it subtype
+     * 
      * @param <T>
      * @param t
      * @param c
@@ -53,15 +54,66 @@ public class Ex104MinMaxAvgStudentGenerics {
 
     /**
      * Can be called by any Type which implements Comparable
+     * 
      * @param <T>
      * @param t
      * @return
      */
     private static <T extends Comparable<T>> T getHeaviestStudentV1(T[] t) {
-       
+
         T max = t[0];
         for (T p : t) {
             if (p.compareTo(max) > 0)
+                max = p;
+        }
+
+        return max;
+
+    }
+
+    /**
+     * Why this method is giving error?
+     * 
+     * 
+     * Here type parameter is T, if we write <Comparator<? extends T> it means that Comparator is supporting only T or its sub type.
+     * Now compare method takes Object obj1 and obj2 as parameters. But have already specified that Comparator is supporting only T or it
+     * subtype not its super type like Object. So comparator is not able to handle 
+     * that is why it won't work.
+     * 
+     * ChatGPT:
+     * By changing Comparator<? extends T> to Comparator<? super T>, the method ensures that the comparator can handle any type that is a
+     * superclass of T. This allows the comparator to compare T objects correctly, fixing the type compatibility issue.
+     * 
+     * @param <T>
+     * @param t
+     * @param c
+     * @return
+     */
+    private static <T> T getHeaviestStudentV3(T[] t, Comparator<? extends T> c) {
+
+        T max = t[0];
+        for (T p : t) {
+            if (c.compare(p, max) > 0)
+                max = p;
+        }
+
+        return max;
+
+    }
+
+    /**
+     * This works.
+     * 
+     * @param <T>
+     * @param t
+     * @param c
+     * @return
+     */
+    private static <T> T getHeaviestStudentV4(T[] t, Comparator<T> c) {
+
+        T max = t[0];
+        for (T p : t) {
+            if (c.compare(p, max) > 0)
                 max = p;
         }
 
