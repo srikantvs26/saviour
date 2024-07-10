63c7e5f66cb9395098719b5d9cd980102cbaaae8
Srikant V S <38664247+srikantvs26@users.noreply.github.com>
2024-07-10 16:58:06 +0000

    kh

--- Java-17/src/com/srikantvs/java8/generics/v2/Example7.java
+++ Java-17/src/com/srikantvs/java8/generics/v2/Example7.java
@@ -9,15 +9,16 @@ class D<X, Y> {
 
 public class Example7 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		D<String, Integer> d1 = new D<>();
-		D<String, Integer> d2 = new D<String, Integer>();
+        D<String, Integer> d1 = new D<>();
+        D<String, Integer> d2 = new D<String, Integer>();
 
-		D<?, Integer> d3 = new D<String, Integer>(); // It works in Diamond operator if we don't specify anything it
-														// takes as Object. ? is
-														// any type so Object it can support.
+        D<?, Integer> d3 = new D<String, Integer>(); // It works in Diamond operator if we don't specify anything it
+                                                     // takes as Object. ? is any type so Object it can support.
 
-	}
+        D<?, ? extends Number> d4 = new D<String, Integer>();
+
+    }
 
 }
